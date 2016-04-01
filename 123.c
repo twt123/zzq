482a52d0136fd6713d46781f69abe442572a1012
zzq <798876134@qq.com>
2016-04-01 15:08:37 +0800

    second commit

--- 123.c
+++ 123.c
@@ -1,9 +1,18 @@
 #include "stdio.h"
 
 int main()
-{
+{	
+	int max(int,int);
 	printf("hello world\n");
-
+	max(2,3);
 	return 0;
+
+}
+
+int max(int a,int b)
+{
+	int c;
+	c=a+b;
+	printf("%d\n",c);
 
 }

74db5baf297f24ec1afa4d125d0e1305cf1f08f5
zzq <798876134@qq.com>
2016-04-02 14:39:47 +0800

    input file

---
+++ test.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main()
+{
+	FILE *fp;
+	char ch,filename[10];
+	scanf("%s",filename);
+	if((fp=fopen(filename,"w+"))==NULL)
+	{
+		printf("cannot open file\n");
+		exit(0);
+	}
+	ch=getchar();
+	ch=getchar();
+	while(ch!='#')
+	{
+		fputc(ch,fp);
+		putchar(ch);
+		ch=getchar();
+	
+	}
+	putchar(10);
+	fclose(fp);
+}
